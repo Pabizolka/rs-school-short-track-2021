4a0e46a67b3eb728e0ed2f1cc2de46e7f0f78063
Pabizolka <1232312009qq@gmail.com>
2021-04-10 16:14:08 +0300

    feat: complete 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const array = str.split('');
+  let count = 1;
+  const len = array.length;
+  let pre;
+  let cur;
+  let res = '';
+  pre = array.shift();
+  for (let i = 1; i < len; i++) {
+    cur = array.shift();
+    if (pre === cur) {
+      count++;
+    } else {
+      res = count === 1 ? `${res}${pre}` : `${res}${count}${pre}`;
+      count = 1;
+    }
+    pre = cur;
+  }
+  res = count === 1 ? `${res}${pre}` : `${res}${count}${pre}`;
+
+  return res === 'undefined' ? '' : res;
 }
 
 module.exports = encodeLine;
