12ac799d23c4f5cdf302d675ed7ac2aa90309674
Pabizolka <1232312009qq@gmail.com>
2021-04-10 08:52:21 +0300

    feat: complete 01-matrix-elements-sum

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,14 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let res = 0;
+  matrix.forEach((array, row) => {
+    array.forEach((element, column) => {
+      res += matrix[(row - 1, column)] === 0 ? 0 : element;
+    });
+  });
+  return res;
 }
 
 module.exports = getMatrixElementsSum;
