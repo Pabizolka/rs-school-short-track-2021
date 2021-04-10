78040ef8ec951ec08b3b828d8a1b3698c0154bac
Pabizolka <1232312009qq@gmail.com>
2021-04-10 09:56:24 +0300

    fix: fix 01

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -17,9 +17,15 @@
 function getMatrixElementsSum(matrix) {
   let res = 0;
   matrix.forEach((array, row) => {
-    array.forEach((element, column) => {
-      res += matrix[(row - 1, column)] === 0 ? 0 : element;
-    });
+    if (row === 0) {
+      array.forEach((element) => {
+        res += element;
+      });
+    } else {
+      array.forEach((element, column) => {
+        res += matrix[row - 1][column] === 0 ? 0 : element;
+      });
+    }
   });
   return res;
 }
